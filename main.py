4885c0cb1d894802d84bf9513b8e8689333106f9
Dzony <donciop123@gmail.com>
2022-06-08 01:12:27 +0200

    final channel checks

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def on_ready():
 
 @client.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
+    if isinstance(error, nextcord.ext.application_checks.errors.ApplicationMissingPermissions):
         await ctx.send(f'You don\'t have {error.missing_permissions} permission to use this command.', delete_after=5)
 
 client.run(os.getenv('TOKEN'))
